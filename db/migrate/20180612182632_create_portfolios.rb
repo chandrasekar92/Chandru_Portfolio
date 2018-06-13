25ce4624c6f959c9dd421ba9844a5a5353664484
Chandrasekar <chandrasekar@housingman.com>
2018-06-13 22:49:37 +0530

    added portfolio items via Resource generator

--- db/migrate/20180612182632_create_portfolios.rb
+++ db/migrate/20180612182632_create_portfolios.rb
@@ -1,10 +1,11 @@
-class CreatePorfolios < ActiveRecord::Migration[5.1]
+class CreatePortfolios < ActiveRecord::Migration[5.1]
   def change
-    create_table :porfolios do |t|
+    create_table :portfolios do |t|
       t.string :title
       t.string :subtitle
       t.text :body
       t.text :main_image
+      t.text :thumb_image
 
       t.timestamps
     end
